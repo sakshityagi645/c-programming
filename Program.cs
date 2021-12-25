8f74fb0a832ad9f09df7aef80657d6ed8fdc6b55
Sakshi Tyagi <55343037+sakshityagi645@users.noreply.github.com>
2021-12-25 22:33:46 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace inheritTwoClass
+{
+    class Mobile
+    {
+        public string brand = "Android";
+        public void Config() 
+        {
+            Console.WriteLine("Hello! welcome to class..");
+        }
+    }
+    class Samsung : Mobile
+    {
+        public string modelName = "Samsung J5";
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Samsung mobile = new Samsung();
+            mobile.Config();
+            Console.WriteLine(mobile.brand + " " + mobile.modelName);
+        }
+    }
+}
