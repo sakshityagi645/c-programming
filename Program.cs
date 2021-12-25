257632c74973506a65112df79c56800f2e743663
Sakshi Tyagi <55343037+sakshityagi645@users.noreply.github.com>
2021-12-25 22:38:22 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,26 +1,49 @@
 ﻿using System;
 
-namespace inheritTwoClass
+namespace EmployeeDetails
 {
-    class Mobile
+    class Employee
     {
-        public string brand = "Android";
-        public void Config() 
+        int id;
+        string name;
+        int age;
+        int mobileno;
+        public void setEmployee(int id, string name, int age, int mobileno)
         {
-            Console.WriteLine("Hello! welcome to class..");
+            this.id = id;
+            this.name = name;
+            this.age = age;
+            this.mobileno = mobileno;
         }
-    }
-    class Samsung : Mobile
-    {
-        public string modelName = "Samsung J5";
-    }
-    class Program
-    {
-        static void Main(string[] args)
+        public void getEmployee()
         {
-            Samsung mobile = new Samsung();
-            mobile.Config();
-            Console.WriteLine(mobile.brand + " " + mobile.modelName);
+            Console.WriteLine("Employee_Id is: {0}", this.id);
+            Console.WriteLine("Employee_Name is: {0}", this.name);
+            Console.WriteLine("Employee_age is: {0}", this.age);
+            Console.WriteLine("Employee_Contact number is: {0}", this.mobileno);
+        }
+
+
+
+        //class Program
+        //{
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Employee_Id: ");
+            int id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Employee_ Name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter Employee_Age: ");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Employee Contact Number: ");
+            int mobileno = int.Parse(Console.ReadLine());
+            Employee obj = new Employee();
+            obj.setEmployee(id, name, age, mobileno);
+            obj.getEmployee();
+            Console.WriteLine("...............");
+            Console.ReadLine();
+
         }
     }
 }
+//}
